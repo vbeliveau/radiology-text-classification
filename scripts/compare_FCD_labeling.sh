b7223d372902685c24b9e7b36a94280a7dbdc8f2
Vincent Beliveau <vincent.beliveau@nru.dk>
2024-09-27 18:26:25 +0200

    cleaning

--- scripts/compare_FCD_labeling.sh
+++ scripts/compare_FCD_labeling.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# For debugging text_utils
-# --bind /proc_data1/bd5/text_utils/src/text_utils/text_utils.py:/venv/lib/python3.10/site-packages/text_utils/text_utils.py \
-
 apptainer run \
 	--net --network none \
 	-H /proc_data1/bd5/nlp:/nlp \
